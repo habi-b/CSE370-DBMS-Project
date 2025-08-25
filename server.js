604c67fdcf19dbb7bd19bb8dd584a7fc7eb5ee7b
ShihabSakib <notsskhan1234@gmail.com>
2025-08-26 02:22:17 +0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const dashboardRoutes = require('./src/routes/dashboardRoutes');
 const goalsRoutes = require('./src/routes/goalsRoutes');
 const loanRoutes = require('./src/routes/loanRoutes');
 const scheduledTransfersRoutes = require('./src/routes/scheduledTransfersRoutes');
-const transactiontagsRoutes = require('./src/routes/transactiontagsRoutes');
+const transactionTagsRoutes = require('./src/routes/transactionTagsRoutes');
 require('dotenv').config(); // Loads environment variables from a .env file
 
 // Import routes
@@ -23,7 +23,7 @@ app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/goals', goalsRoutes);
 app.use('/api/loans', loanRoutes);
 app.use('/api/schedules', scheduledTransfersRoutes);
-app.use('/api/transaction-tags', transactiontagsRoutes);
+app.use('/api/transaction-tags', transactionTagsRoutes);
 
 // the port for the server to listen on
 const PORT = process.env.PORT || 3000;
